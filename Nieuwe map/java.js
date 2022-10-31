56032faab3e1702db3a6298c6d6cb0fd2e61cb62
mo <114405378+mo3922@users.noreply.github.com>
2022-10-31 14:53:22 +0100

    Add files via upload

---
+++ Nieuwe map/java.js
@@ -0,0 +1,31 @@
+function myFunction() {
+    const background = document.body.style.backgroundColor;
+    if(background  === "lightblue"){ 
+      document.body.style.backgroundColor = "white";
+    } else {
+     document.body.style.backgroundColor = "lightblue";
+    }}
+
+    function my1Function() {
+        const background = document.body.style.backgroundColor;
+        if(background  === "pink"){ 
+          document.body.style.backgroundColor = "white";
+        } else {
+         document.body.style.backgroundColor = "pink";
+        }}
+
+        function my2Function() {
+            const background = document.body.style.backgroundColor;
+            if(background  === "red"){ 
+              document.body.style.backgroundColor = "white";
+            } else {
+             document.body.style.backgroundColor = "red";
+            }}      
+
+            function my3Function() {
+                const background = document.body.style.backgroundColor;
+                if(background  === "yellow"){ 
+                  document.body.style.backgroundColor = "white";
+                } else {
+                 document.body.style.backgroundColor = "yellow";
+                }}            
